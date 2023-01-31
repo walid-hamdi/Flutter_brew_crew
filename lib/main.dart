fe4257c9ab8941ef877bd872bc0f01473352789d
walid hamdi <walidhamdidev@gmail.com>
2023-01-31 14:06:33 +0100

    secure firebase keys

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'firebase_options.dart';
 import 'screens/wrapper.dart';
 import 'services/auth.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
